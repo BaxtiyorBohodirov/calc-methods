eb5aa5ccc5b547b9dad08bf27fab21a3fd2660e5
BaxtiyorBohodirov <bohodirovbaxtiyor19@gmail.com>
2023-12-06 17:16:23 +0500

    add new methods to calc

---
+++ src/CalcMethods/CalcMethods.php
@@ -0,0 +1,14 @@
+<?php
+
+class CalcMethods {
+
+
+    public  function multiply($number1, $number2) {
+        return $number1 * $number2;
+    }
+
+    public function add($number1, $number2) {
+        return $number1 + $number2;
+    }
+}
+?>
